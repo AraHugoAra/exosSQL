b05d95412a2342c79f985f7e6edf1ec90b062fd0
HugoA <aragones.hugo@gmail.com>
2022-12-06 14:50:56 +0100

    exo 6

--- SQL1.1-07-Requêtes-SELECT-complexes.sql
+++ SQL1.1-07-Requêtes-SELECT-complexes.sql
@@ -1,11 +1,19 @@
 /* Le total des paiements effectués de chaque client (numéro, nom et pays) américain, allemand ou français de plus de 50000$ trié par pays puis par total des paiements décroissant */
-
-
 /* RESULTAT ==> 38 lignes / 146 / 130305.35 */
 
-
+SELECT customers.customerNumber, customerName, country, SUM(amount) AS totalCredit FROM `customers`
+	JOIN payments ON customers.customerNumber = payments.customerNumber
+    	WHERE country IN ("USA", "France", "Germany")
+        GROUP BY payments.customerNumber
+        	HAVING totalCredit > 50000
+        		ORDER BY country, totalCredit DESC;
 
 /* Le montant total de chaque commande (numéro et date) des clients New-Yorkais (nom) trié par nom du client puis par date de commande */
-
-
 /* RESULTAT ==> 16 lignes / Classic Legends / 10115 / 21665.98 */
+
+SELECT SUM(orderdetails.priceEach * orderdetails.quantityOrdered) AS totalAmount, orders.orderNumber, orders.orderDate, customerName FROM `customers`
+	JOIN orders ON customers.customerNumber = orders.customerNumber
+    JOIN orderdetails ON orderdetails.orderNumber = orders.orderNumber
+    	WHERE city LIKE "nyc"
+    	GROUP BY orders.orderNumber
+        	ORDER BY customerName, orders.orderDate;
